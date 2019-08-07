ead9d2d0b3e4510490b6b09fd2bc5ab4816901c6
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 19:13:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
   counter = 0
   numbers = [1, 2, 3]
-  while counter square_array(array) = (numbers ** 2)
+  while counter < array.length do
+    puts square_array(array) = (numbers ** 2)
 end
